7ef35474280b331457d3e2cf7f4c5b38c51071f3
HawkinsJM <hawkins.jeffm@gmail.com>
2025-10-15 20:23:11 -0700

    changed text

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function checkForAstronauts(button) {
       // Update the Message
       document.getElementById(
         "status"
-      ).innerHTML = `Whoawhoa there are ${data.number} people in space rn!!`;
+      ).innerHTML = `Whoawhfasdfoa there are ${data.number} people in space rn!!`;
 
       // Add each Astronaut to their craft
       var astros = data.people;
